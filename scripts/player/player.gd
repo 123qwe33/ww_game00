ebfad0de3a104b2648122c9ef59aa078dcb46ebb
Geoff Rogers <geoffrogersdesign@gmail.com>
2025-04-18 18:59:44 -0400

    Making Sure in Air Long Enough to Trigger Jump State

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -4,18 +4,24 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+const AIR_THRESHOLD = 0.15  # Time in seconds to ignore brief air states
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var air_time = 0.0  # Tracks how long player has been in the air
 
 func _physics_process(delta):
-	# Add the gravity.
+	# Track air time and handle gravity
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		air_time += delta
+	else:
+		air_time = 0.0
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		air_time = AIR_THRESHOLD  # Immediately consider this a real jump
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -35,8 +41,8 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED) # Simple friction/stop
 	
 	# Handle animations based on state
-	if not is_on_floor():
-		# Play jump animation when in the air
+	if not is_on_floor() and air_time >= AIR_THRESHOLD:
+		# Only play jump animation if we've been in the air longer than the threshold
 		sprite.play("jump")
 	elif direction:
 		sprite.play("walk")
