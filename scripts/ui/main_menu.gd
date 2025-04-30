54a9fd2abe357d2558a98414af36e6cd05c514e7
Geoff Rogers <geoffrogersdesign@gmail.com>
2025-04-30 00:37:55 -0400

    Menus Progress

--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -6,3 +6,6 @@ func _on_start_button_pressed() -> void:
 		var error = get_tree().change_scene_to_file("res://scenes/levels/level_01.tscn")
 		if error != OK:
 				print("Error changing scene: ", error)
+
+func _on_quit_to_desktop_button_pressed():
+	get_tree().quit()
