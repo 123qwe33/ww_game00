575f469f533d4d7f903a188052b707e410970c39
Geoff Rogers <geoffrogersdesign@gmail.com>
2025-05-23 00:06:09 -0400

    Fixed Loading Null Issue

--- scripts/autoloads/sound_manager.gd
+++ scripts/autoloads/sound_manager.gd
@@ -1,14 +1,15 @@
 extends Node
 
 var hover_sounds = [
-    preload("res://assets/audio/sfx/ui/bing.wav")
+	preload("res://assets/audio/sfx/ui/bing.wav")
 ]
 
 @onready var audio_player = AudioStreamPlayer.new()
 
 func _ready():
-    add_child(audio_player)
+	add_child(audio_player)
 
 func play_hover_sound():
-    audio_player.stream = hover_sounds[0]
-    audio_player.play()
+	if (audio_player):
+		audio_player.stream = hover_sounds[0]
+		audio_player.play()
