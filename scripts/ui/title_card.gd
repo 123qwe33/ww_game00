44be1df944ca83e1b6f7997e6d85f4594dfe4743
Geoff Rogers <geoffrogersdesign@gmail.com>
2025-05-28 19:13:24 -0400

    Setting Custom Text

--- scripts/ui/title_card.gd
+++ scripts/ui/title_card.gd
@@ -1,12 +1,13 @@
 extends Control
 
 @onready var TitleCardCanvas = TitleCard.get_child(0)
-# @onready var TitleCardLabel = TitleCard.find_child("", Label, true)
+@onready var TitleCardLabel = TitleCard.find_child("Label", true, true)
 
 func _ready():
 	TitleCardCanvas.hide()
 
 func display(title):
+	TitleCardLabel.text = title
 	TitleCardCanvas.show()
 	# Hide it after a short delay
 	await get_tree().create_timer(2.0).timeout
