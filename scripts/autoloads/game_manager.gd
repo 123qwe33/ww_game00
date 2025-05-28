1c3b81e01ec86f06a1ca87042fa092f8fff8659f
Geoff Rogers <geoffrogersdesign@gmail.com>
2025-05-28 18:35:48 -0400

    Moved Game Starting Logic Into Game Manager

--- scripts/autoloads/game_manager.gd
+++ scripts/autoloads/game_manager.gd
@@ -14,6 +14,13 @@ const MAIN_MENU_SCENE = "res://scenes/ui/main_menu.tscn"
 # Signal for when player dies - carry death cause for potential future use
 signal player_died(cause: DeathCause)
 
+func start_game():
+	# Change the current scene to the first level
+	var error = get_tree().change_scene_to_file("res://scenes/levels/level_01.tscn")
+	if error != OK:
+		print("Error changing scene: ", error)
+	SoundManager.change_track("quiet_forest")
+
 # Handles player death with any cause
 func kill_player(cause: DeathCause = DeathCause.CUSTOM) -> void:
 	# Emit signal for anything that needs to react to player death
--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -23,12 +23,8 @@ func _on_start_button_pressed() -> void:
 		
 		# Wait for the sound to finish before changing scenes
 		await get_tree().create_timer(1.0).timeout  # Adjust timing as needed for your sound
-	
-		# Change the current scene to the first level
-		var error = get_tree().change_scene_to_file("res://scenes/levels/level_01.tscn")
-		if error != OK:
-				print("Error changing scene: ", error)
-		SoundManager.change_track("quiet_forest")
+
+		GameManager.start_game()  # Start the game
 
 func _on_quit_to_desktop_button_pressed():
 		get_tree().quit()
