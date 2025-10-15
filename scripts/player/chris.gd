3ffaa9f8bbe93abb26345dcf935386df200a4224
Geoff Rogers <geoffrogersdesign@gmail.com>
2025-10-15 17:48:47 -0400

    Added NPC & Chris
    Chris walks left until close to player

--- scripts/player/chris.gd
+++ scripts/player/chris.gd
@@ -1,3 +1,28 @@
 extends NPC
 
-	
+@export var stop_distance: float = 500.0
+
+var player: Character = null
+
+func _ready():
+	super._ready()
+	# Find the player in the scene
+	player = get_tree().get_first_node_in_group("player")
+	if not player:
+		push_warning("Chris: Could not find player in scene")
+
+func _physics_process(delta):
+	# Only move if we have a valid player reference
+	if player:
+		var distance_to_player = global_position.distance_to(player.global_position)
+
+		# Walk left if we're too far from the player
+		if distance_to_player > stop_distance:
+			movement_direction = -1.0
+		else:
+			movement_direction = 0.0
+	else:
+		movement_direction = 0.0
+
+	# Let Character handle all the physics, gravity, animations, etc.
+	super._physics_process(delta)
--- scripts/player/npc.gd
+++ scripts/player/npc.gd
@@ -1,2 +1,13 @@
 extends Character
 class_name NPC
+
+var movement_direction: float = 0.0
+
+func _ready():
+	super._ready()
+	# NPCs don't use player input
+	input_enabled = false
+
+# Override to return NPC-controlled direction instead of input
+func get_movement_direction() -> float:
+	return movement_direction
